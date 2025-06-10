64454624d88604bbdd1741977e1ae70557def1b8
JPH <jphamblett@googlemail.com>
2025-06-10 13:57:44 +0100

    AMMEND new hello

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("JPH"));
+        System.out.println(sayHelloToSomeone("Stefan"));
         System.out.println(sayGoodbye());
     }
 
